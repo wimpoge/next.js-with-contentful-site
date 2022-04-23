d1f171202b4095870828f2470a2bc10bbca19b65
wimpoge <34478110+wimpoge@users.noreply.github.com>
2022-04-24 01:49:24 +0700

    404

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 4000);
+
+  }, [])
+
+  return (
+    <div className="not-found">
+        <h1>404</h1>
+        <p>Page not found</p>
+        <p>Redirecting to <Link href ="/">Homepage</Link></p>
+
+        <style jsx>{`
+        .not-found {
+          background: #fff;
+          padding: 30px;
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+          transform: rotateZ(-1deg);
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  )
+}
